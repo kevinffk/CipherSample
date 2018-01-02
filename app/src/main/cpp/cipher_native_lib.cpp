191e8e801fb36b02f00e1d72fe344e0b0ea3f150
kevinffk <kevinffk@126.com>
2018-01-03 00:42:16 +0800

    add the funtion crypt md5 of jni learn how to call c function how to use jni debug tool lldb

--- app/src/main/cpp/cipher_native_lib.cpp
+++ app/src/main/cpp/cipher_native_lib.cpp
@@ -4,6 +4,7 @@
 #include "cipher_native_lib.h"
 
 #include "md5.h"
+#include "my_utils.h"
 #include <string.h>
 
 
@@ -20,25 +21,28 @@ Java_com_kv_ciphersample_CipherNativeLib_cryptMd5(JNIEnv *env, jclass type, jstr
     const char *source = env->GetStringUTFChars(source_, 0);
 
     int len = strlen(source);
-    int i;
 
     unsigned char encrypt[len];//21232f297a57a5a743894a0e4a801fc3
     unsigned char decrypt[16];
 
-    for (i = 0; i<len; i++) {
+    for (int i = 0; i<len; i++) {
         encrypt[i]=*source;
         source++;
     }
 
     MD5_CTX md5;
     MD5Init(&md5);
-    MD5Update(&md5,encrypt,strlen((char *)source));
+    MD5Update(&md5,encrypt,len);
     MD5Final(&md5,decrypt);
 
-    LOGE("加密前:%s\n加密后:",encrypt);
+    char target[32];
+
+    ByteToHexStr(decrypt, target, 16);
+
+    LOGE("加密前:%s\n加密后:%s",encrypt, target);
 
     env->ReleaseStringUTFChars(source_, source);
 
-    return env->NewStringUTF("abc");
+    return env->NewStringUTF(target);
 }
 }
---
+++ app/src/main/cpp/my_utils.h
@@ -0,0 +1,7 @@
+#include <string.h>
+
+void ByteToHexStr(const unsigned char* source, char* dest, int sourceLen);
+
+void Char2Hex(unsigned char ch, char *szHex);
+
+void CharStr2HexStr( unsigned char *pucCharStr, int iSize,  char *pszHexStr);
